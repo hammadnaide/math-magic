73fad86dd74370449af89a205234444275a07240
hammadnaide <hammadnaide@gmail.com>
2025-03-22 16:57:21 +0000

    Add 17.rs

---
+++ src/17.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Define a function to perform addition
+    fn add(x: isize, y: isize) -> isize {
+        x + y
+    }
+
+    // Call the function with two integers as arguments
+    let result = add(10, 20);
+    println!("The sum is: {}", result);
+}
