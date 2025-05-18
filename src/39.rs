2b0495f60aa9295184e2defad90d55d2ac2879e8
hammadnaide <hammadnaide@gmail.com>
2025-05-18 03:55:33 +0000

    Add 39.rs

---
+++ src/39.rs
@@ -0,0 +1,7 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+/// This function reads a file containing a list of integers and converts each integer to its binary representation.
+///
+/// Example usage:
+/// 
